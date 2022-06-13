306633e8060a8b5c0972bfd9673f2e0c7fcbb967
Dinh Nguyen <cnguyen@neusta.de>
2022-06-13 23:06:59 +0200

    Search

--- src/components/Layout/common/Header/index.js
+++ src/components/Layout/common/Header/index.js
@@ -1,5 +1,10 @@
 import classNames from "classnames/bind";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleXmark, faMagnifyingGlass, faSpinner } from "@fortawesome/free-solid-svg-icons";
+
 import styles from "./Header.module.scss";
+import images from "@/assets/images";
 
 const cx = classNames.bind(styles);
 
@@ -8,7 +13,21 @@ function Header() {
         <header className={cx("wrapper")}>
             <div className={cx("inner")}>
                 {/*Logo*/}
+                <img src={images.logo} alt="Tiktok"></img>
                 {/*Search*/}
+                <div className={cx("search")}>
+                    <input placeholder="Search accounts and videos" spellCheck={false}></input>
+                    <button className={cx("clear")}>
+                        <FontAwesomeIcon icon={faCircleXmark} />
+                    </button>
+                    {/* Loading */}
+                    <FontAwesomeIcon className={cx("loading")} icon={faSpinner} />
+                    <button className={cx("search-btn")}>
+                        {/* Search */}
+                        <FontAwesomeIcon icon={faMagnifyingGlass} />
+                    </button>
+                    <div className={cx("actions")}></div>
+                </div>
             </div>
         </header>
     );
