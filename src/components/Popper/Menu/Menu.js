71f4c72c26a0732e22c42e7b4dab2c6018fd206b
cnguyen <cnguyen@neusta.de>
2022-07-22 11:40:31 +0200

    refactor

--- src/components/Popper/Menu/Menu.js
+++ src/components/Popper/Menu/Menu.js
@@ -36,6 +36,21 @@ function Menu({ children, items = [], hideOnClick = false, onChange = defaultFn 
         });
     };
 
+    const handleResetMenu = () => {
+        setHistory((prev) => prev.slice(0, prev.length - 1));
+    };
+
+    const renderMenu = (attrs) => (
+        <div className={cx("menu-list")} tabIndex="-1" {...attrs}>
+            <PopperWrapper className={cx("menu-popper")}>
+                {history.length > 1 && <MenuHeader title={current.title} onBack={handleResetMenu} />}
+                <div className={cx("menu-body")}>{renderItems()}</div>
+            </PopperWrapper>
+        </div>
+    );
+
+    const handleResetToFirstPage = () => setHistory((prev) => prev.slice(0, 1));
+
     return (
         <Tippy
             interactive
@@ -43,22 +58,8 @@ function Menu({ children, items = [], hideOnClick = false, onChange = defaultFn 
             offset={[12, 8]}
             placement="bottom-end"
             hideOnClick={hideOnClick}
-            render={(attrs) => (
-                <div className={cx("menu-list")} tabIndex="-1" {...attrs}>
-                    <PopperWrapper className={cx("menu-popper")}>
-                        {history.length > 1 && (
-                            <MenuHeader
-                                title={current.title}
-                                onBack={() => {
-                                    setHistory((prev) => prev.slice(0, prev.length - 1));
-                                }}
-                            />
-                        )}
-                        <div className={cx("menu-body")}>{renderItems()}</div>
-                    </PopperWrapper>
-                </div>
-            )}
-            onHide={() => setHistory((prev) => prev.slice(0, 1))}
+            render={renderMenu}
+            onHide={handleResetToFirstPage}
         >
             {children}
         </Tippy>
