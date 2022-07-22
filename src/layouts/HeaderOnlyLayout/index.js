c690275da6542ec7132eeb8bec5e77954653024b
cnguyen <cnguyen@neusta.de>
2022-07-22 11:21:52 +0200

    convert index files to original

--- src/layouts/HeaderOnlyLayout/index.js
+++ src/layouts/HeaderOnlyLayout/index.js
@@ -1,14 +1,1 @@
-import Header from "../common/Header";
-
-function HeaderOnlyLayout({ children }) {
-    return (
-        <div>
-            <Header />
-            <div className="container">
-                <div className="content">{children}</div>
-            </div>
-        </div>
-    );
-}
-
-export default HeaderOnlyLayout;
+export { default } from "./HeaderOnlyLayout";
